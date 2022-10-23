188f7921e15c211bae8df90cd0e0a395111ef1b3
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2022-10-23 18:21:08 -0500

    Prueba unitaria para E:\Documentos\Pedro Luis\Portafolio de proyectos\TiendaDonaSusyGitHub\tds-microservice-product\src\main\java\co\com\viveres\susy\microserviceproduct\config\DockerSecretProcessor.java

--- src/main/java/co/com/viveres/susy/microserviceproduct/config/DockerSecretProcessor.java
+++ src/main/java/co/com/viveres/susy/microserviceproduct/config/DockerSecretProcessor.java
@@ -1,6 +1,5 @@
 package co.com.viveres.susy.microserviceproduct.config;
 
-import org.apache.logging.log4j.LogManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
