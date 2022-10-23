ad9b118555f0de23af3ed45282d62a2456768428
pedro-chavezcastro <pedro.chavezcastro@outlook.com>
2022-10-23 14:50:29 -0500

    Update DockerSecretProcessor.java
    Solución a UN error de Sonar - Pendiente solución a lo demas

--- src/main/java/co/com/viveres/susy/microserviceproduct/config/DockerSecretProcessor.java
+++ src/main/java/co/com/viveres/susy/microserviceproduct/config/DockerSecretProcessor.java
@@ -13,6 +13,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DockerSecretProcessor implements EnvironmentPostProcessor {
 
@@ -27,25 +28,23 @@ public class DockerSecretProcessor implements EnvironmentPostProcessor {
       Path bindPath = Paths.get(bindPathPpty);
       if (Files.isDirectory(bindPath)) {
         Map<String, Object> dockerSecrets;
-        try {
-          dockerSecrets =
-              Files.list(bindPath)
-                  .collect(
-                      Collectors.toMap(
-                          path -> {
-                            File secretFile = path.toFile();
-                            return "tds-config-" + secretFile.getName();
-                          },
-                          path -> {
-                            File secretFile = path.toFile();
-                            try {
-                              byte[] content = FileCopyUtils.copyToByteArray(secretFile);
-                              return new String(content);
-                            } catch (IOException e) {
-                              throw new RuntimeException(e);
-                            }
-                          }
-                      ));
+        try (Stream<Path> pathStream = Files.list(bindPath)) {
+          dockerSecrets = pathStream.collect(
+              Collectors.toMap(
+                  path -> {
+                    File secretFile = path.toFile();
+                    return "tds-config-" + secretFile.getName();
+                  },
+                  path -> {
+                    File secretFile = path.toFile();
+                    try {
+                      byte[] content = FileCopyUtils.copyToByteArray(secretFile);
+                      return new String(content);
+                    } catch (IOException e) {
+                      throw new RuntimeException(e);
+                    }
+                  }
+              ));
         } catch (IOException e) {
           throw new RuntimeException(e);
         }
